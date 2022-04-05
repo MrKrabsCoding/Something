a1bda03b7a3ae9f446ae6dee014f507a2a495927
MrKrabsCoding <89555978+MrKrabsCoding@users.noreply.github.com>
2022-04-05 17:34:57 +0000

    Added random functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+int multiply(int x, int y) { return x * y; }
+
+int add(int x, int y) { return x + y; }
+
+int subtract(int x, int y) { return x - y; }
+
+
+int main(){
+
+	return 0;
 }
